e41223b0ac4d4ca742e140d56d5151e8be6ad1c5
Elias Horner <elias.horner@student.htldornbirn.at>
2020-01-03 15:35:28 +0100

    change position of camera

--- client/client/state/IngameState.cpp
+++ client/client/state/IngameState.cpp
@@ -10,49 +10,43 @@ namespace card {
 	IngameState::IngameState(StateManager& stateManager, egui::MasterRenderer& eguiRenderer) :
 			State(stateManager),
 			projectionMatrix(45, 1, 100),
-			viewport({0, 0, 10}, {-PI / 8, 0, 0}),
+			viewport({0, 0.2f, 7.0f}, {-0.5f, 0, 0}),
 			eguiRenderer(eguiRenderer),
 			sceneRenderer(projectionMatrix, viewport, eguiRenderer) {
+	}
+
+	std::string vec3ToString(glm::vec3 vec) {
+		return "(" + std::to_string(vec.x) + " " + std::to_string(vec.y) + " " + std::to_string(vec.z) + ")";
 	}
 
 	void IngameState::updateAndRender(float delta) {
 		// render game scene
 		if(egui::getInputHandler().isKeyDown(KEY_1)) {
 			viewport.setPosition(viewport.getPosition() + glm::vec3(0, 0, 0.05f));
+			std::cout << vec3ToString(viewport.getPosition()) << std::endl;
 		}
 		if(egui::getInputHandler().isKeyDown(KEY_2)) {
 			viewport.setPosition(viewport.getPosition() - glm::vec3(0, 0, 0.05f));
+			std::cout << vec3ToString(viewport.getPosition()) << std::endl;
 		}
 		if(egui::getInputHandler().isKeyDown(KEY_3)) {
 			viewport.setPosition(glm::vec3(0, 0, 5));
+			std::cout << vec3ToString(viewport.getPosition()) << std::endl;
 		}
 
 		if(egui::getInputHandler().isKeyDown(KEY_4)) {
 			viewport.setRotation(viewport.getRotation() + glm::vec3(0.01f, 0, 0));
+			std::cout << vec3ToString(viewport.getRotation()) << std::endl;
 		}
 		if(egui::getInputHandler().isKeyDown(KEY_5)) {
 			viewport.setRotation(viewport.getRotation() - glm::vec3(0.01f, 0, 0));
+			std::cout << vec3ToString(viewport.getRotation()) << std::endl;
 		}
 		if(egui::getInputHandler().isKeyDown(KEY_6)) {
 			viewport.setRotation(glm::vec3(-PI / 8, 0, 0));
+			std::cout << vec3ToString(viewport.getRotation()) << std::endl;
 		}
 
-/*		static std::function<void(void)> addFct = [this]() {
-			auto& game = getStateMananger().getGameFacade()->getGame();
-			game.getOpponents()[2]->playCardLocal({1}, game.playCardStack, false);
-
-			threadUtils_invokeIn(5000, addFct);
-		};
-		static bool wasSet = false;
-		if(! wasSet) {
-			addFct();
-			wasSet = true;
-		}*/
-
-		std::shared_ptr<AbstractGameFacade> gameFacade = getStateMananger().getGameFacade();
-
-
-		sceneRenderer.setGame(gameFacade->getGame());
 		sceneRenderer.render();
 
 	}
