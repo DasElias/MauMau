d1086a2218a34dc15c738285c5d74f53442b8d3f
Elias Horner <elias.horner@student.htldornbirn.at>
2020-01-08 19:13:52 +0100

    remove unnecessary parameter in SimpleTexture

--- client/client/renderingModel/SimpleTexture.cpp
+++ client/client/renderingModel/SimpleTexture.cpp
@@ -20,8 +20,6 @@ namespace card {
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		//TODO what does this line do`?
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 7);
 
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
 		glGenerateMipmap(GL_TEXTURE_2D);
