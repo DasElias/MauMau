c4f31844d23141f2e8372ac805309e8c14eff322
Elias Horner <elias.horner@student.htldornbirn.at>
2020-01-18 00:21:39 +0100

    wrap ServerApplication in try-catch-block with logging

--- server/server/ServerApplication.cpp
+++ server/server/ServerApplication.cpp
@@ -16,10 +16,20 @@ int main() {
 	initDaemon();
 	initLogger("~/latest.log", "~/fatal.log");
 
-	auto packetTransmitter = std::make_shared<ServerPacketTransmitter>();
-	ba::io_context ioc;
-	RoomManager roomManager(packetTransmitter);
-	Acceptor acceptor(ioc, packetTransmitter, roomManager);
+	try {	
+		auto packetTransmitter = std::make_shared<ServerPacketTransmitter>();
+		ba::io_context ioc;
+		RoomManager roomManager(packetTransmitter);
+		Acceptor acceptor(ioc, packetTransmitter, roomManager);
 
-	ioc.run();
+		ioc.run();
+	} catch(boost::system::system_error e) {
+		log(LogSeverity::FATAL, std::string(e.what()) + "(" + std::to_string(e.code().value()) + ")");
+	} catch(std::exception e) {
+		log(LogSeverity::FATAL, e.what());
+	} catch(...) {
+		log(LogSeverity::FATAL, "An unknown exception has happened during network initializing.");
+	}
+	
+
 }
