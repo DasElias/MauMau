029e4e16ed36b5d23c0eafc89d5986e4ebb56cb3
Elias Horner <elias.horner@student.htldornbirn.at>
2020-01-24 19:17:51 +0100

    change CardAnimationDuration

--- shared/shared/model/CardAnimationDuration.h
+++ shared/shared/model/CardAnimationDuration.h
@@ -2,12 +2,12 @@
 #include <cstddef>
 
 namespace card {
-	static int const PLAY_DURATION_MS = 1500;
-	static int const DRAW_DURATION_MS = 1500;
+	static int const PLAY_DURATION_MS = 1250;
+	static int const DRAW_DURATION_MS = 1250;
 	static int const DRAW_MULTIPLE_DELAY_BETWEEN_CARDS_MS = 500;
-	static int const SKIP_ANIMATION_DURATION_MS = 1500;
+	static int const SKIP_ANIMATION_DURATION_MS = PLAY_DURATION_MS;
 	// card is drawed and played in the same turn
-	static int const DELAY_BETWEEN_DRAW_AND_PLAY = 1000;
+	static int const DELAY_BETWEEN_DRAW_AND_PLAY = 750;
 
 	static int const INITIAL_DRAW_DELAY_BETWEEN_CARDS_MS = 250;
 	static int const INITIAL_DRAW_DURATION_PER_CARD_MS = 1000;
