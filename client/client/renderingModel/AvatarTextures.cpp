e0385797c1323029dff9fe1bf90ee9cb3478e7a2
Elias Horner <elias.horner@student.htldornbirn.at>
2020-01-28 20:11:13 +0100

    load avatar textures from memory

--- client/client/renderingModel/AvatarTextures.cpp
+++ client/client/renderingModel/AvatarTextures.cpp
@@ -6,26 +6,54 @@
 #include "SimpleTextureFactory.h"
 #include <stdexcept>
 #include <boost/filesystem.hpp>
+#include <res/avatars/Avatars.png.h>
 
 namespace card {
 	AvatarTextures::AvatarTextures() :
-			avatarNotFoundTexture(SimpleTextureFactory().setMinFilter(TextureMinFilter::LINEAR_MIPMAP_LINEAR).loadFromFile(getApplicationFolder() + "\\resources\\avatars\\notfound.png")),
+			avatarNotFoundTexture(SimpleTextureFactory().setMinFilter(TextureMinFilter::LINEAR_MIPMAP_LINEAR).loadFromMemory(avatar_0, avatar_0_size)),
 			width(-1),
 			height(-1) {
 
 		initTextures();
 	}
 	void card::AvatarTextures::initTextures() {
-		std::string const folder = getApplicationFolder() + "\\resources\\avatars\\";
-		auto const allAvatars = getAllAvatars();
+		static std::vector<std::tuple<Avatar, const unsigned char*, std::size_t>> const avatarImages{{
+			{1, avatar_1, avatar_1_size},
+			{2, avatar_2, avatar_2_size},
+			{3, avatar_3, avatar_3_size},
+			{4, avatar_4, avatar_4_size},
+			{5, avatar_5, avatar_5_size},
+			{6, avatar_6, avatar_6_size},
+			{7, avatar_7, avatar_7_size},
+			{8, avatar_8, avatar_8_size},
+			{9, avatar_9, avatar_9_size},
+			{10, avatar_10, avatar_10_size},
+			{11, avatar_11, avatar_11_size},
+			{12, avatar_12, avatar_12_size},
+			{13, avatar_13, avatar_13_size},
+			{14, avatar_14, avatar_14_size},
+			{15, avatar_15, avatar_15_size},
+			{16, avatar_16, avatar_16_size},
+			{17, avatar_17, avatar_17_size},
+			{18, avatar_18, avatar_18_size},
+			{19, avatar_19, avatar_19_size},
+			{20, avatar_20, avatar_20_size},
+			{21, avatar_21, avatar_21_size},
+			{22, avatar_22, avatar_22_size},
+		}};
+		
+		if(avatarImages.size() != getAllAvatars().size()) {
+			throw std::runtime_error("Avatars couldn't be loaded correctly");
+		}
 
-		for(auto& avatar : allAvatars) {
-			std::string path = folder + std::to_string(avatar) + ".png";
-			if(! boost::filesystem::exists(path)) continue;
+		for(auto& avatarTuple : avatarImages) {
+			Avatar avatar = std::get<0>(avatarTuple);
+			const unsigned char* imgData = std::get<1>(avatarTuple);
+			std::size_t imgDataSize = std::get<2>(avatarTuple);
 
 			SimpleTexture tex = SimpleTextureFactory()
 				.setMinFilter(TextureMinFilter::LINEAR_MIPMAP_LINEAR)
-				.loadFromFile(path);
+				.loadFromMemory(imgData, imgDataSize);
 
 			std::int32_t localWidth = tex.getWidth();
 			std::int32_t localHeight = tex.getHeight();
---
+++ client/res/include/res/avatars/Avatars.png.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <cstddef>
+
+extern const size_t avatar_0_size;
+extern const unsigned char avatar_0[];
+
+extern const size_t avatar_1_size;
+extern const unsigned char avatar_1[];
+
+extern const size_t avatar_2_size;
+extern const unsigned char avatar_2[];
+
+extern const size_t avatar_3_size;
+extern const unsigned char avatar_3[];
+
+extern const size_t avatar_4_size;
+extern const unsigned char avatar_4[];
+
+extern const size_t avatar_5_size;
+extern const unsigned char avatar_5[];
+
+extern const size_t avatar_6_size;
+extern const unsigned char avatar_6[];
+
+extern const size_t avatar_7_size;
+extern const unsigned char avatar_7[];
+
+extern const size_t avatar_8_size;
+extern const unsigned char avatar_8[];
+
+extern const size_t avatar_9_size;
+extern const unsigned char avatar_9[];
+
+
+extern const size_t avatar_10_size;
+extern const unsigned char avatar_10[];
+
+extern const size_t avatar_11_size;
+extern const unsigned char avatar_11[];
+
+extern const size_t avatar_12_size;
+extern const unsigned char avatar_12[];
+
+extern const size_t avatar_13_size;
+extern const unsigned char avatar_13[];
+
+extern const size_t avatar_14_size;
+extern const unsigned char avatar_14[];
+
+extern const size_t avatar_15_size;
+extern const unsigned char avatar_15[];
+
+extern const size_t avatar_16_size;
+extern const unsigned char avatar_16[];
+
+extern const size_t avatar_17_size;
+extern const unsigned char avatar_17[];
+
+extern const size_t avatar_18_size;
+extern const unsigned char avatar_18[];
+
+extern const size_t avatar_19_size;
+extern const unsigned char avatar_19[];
+
+
+extern const size_t avatar_20_size;
+extern const unsigned char avatar_20[];
+
+extern const size_t avatar_21_size;
+extern const unsigned char avatar_21[];
+
+extern const size_t avatar_22_size;
+extern const unsigned char avatar_22[];
