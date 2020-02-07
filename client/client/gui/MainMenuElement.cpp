d0d93a7b49fac4c4a8fccb914680c677dd2730c1
Elias Horner <elias.horner@student.htldornbirn.at>
2020-02-07 14:41:41 +0100

    add hover background for buttons in MainMenuElement

--- client/client/gui/MainMenuElement.cpp
+++ client/client/gui/MainMenuElement.cpp
@@ -16,17 +16,23 @@ namespace card {
 		singlePlayerBtn->setBackground(std::make_shared<egui::TexturedBackground>(
 			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\singlePlayer.png")
 		));
+		singlePlayerBtn->setHoveredBackground(std::make_shared<egui::TexturedBackground>(
+			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\singlePlayerHover.png")
+		), egui::RenderMode::RENDER_EXCLUSIVELY);
 		this->createOnlineRoomBtn = std::make_shared<egui::AspectRatioElement>(1.0f);
 		createOnlineRoomBtn->setBackground(std::make_shared<egui::TexturedBackground>(
 			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\createOnlineRoom.png")
 		));
-		/*createOnlineRoomBtn->setHoveredBackground(std::make_shared<egui::TexturedBackground>(
+		createOnlineRoomBtn->setHoveredBackground(std::make_shared<egui::TexturedBackground>(
 			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\createOnlineRoomHover.png")
-			), egui::RenderMode::RENDER_EXCLUSIVELY);*/
+		), egui::RenderMode::RENDER_EXCLUSIVELY);
 		this->joinOnlineRoomBtn = std::make_shared<egui::AspectRatioElement>(1.0f);
 		joinOnlineRoomBtn->setBackground(std::make_shared<egui::TexturedBackground>(
 			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\joinOnlineRoom.png")
 		));
+		joinOnlineRoomBtn->setHoveredBackground(std::make_shared<egui::TexturedBackground>(
+			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\joinOnlineRoomHover.png")
+		), egui::RenderMode::RENDER_EXCLUSIVELY);
 
 		std::shared_ptr<egui::HBox> btnBar(new egui::HBox({singlePlayerBtn, joinOnlineRoomBtn,  createOnlineRoomBtn}));
 		btnBar->setSpaceBetweenElements({SPACE_BETWEEN_BUTTONS, RelativityMode::RELATIVE_ON_SCREEN});
@@ -40,7 +46,6 @@ namespace card {
 			btn->setBorder(std::make_shared<egui::Border>(egui::Color(0.71f, 0.71f, 0.71f), egui::BorderType::SOLID, 1.0f));
 			btn->setMaxDimension({0.1f, RelativityMode::RELATIVE_ON_SCREEN}, {0.3f, RelativityMode::RELATIVE_ON_SCREEN});
 		}
-
 		this->infoBtn = std::make_shared<egui::AspectRatioElement>(1.0f);
 
 		this->addChildElement(infoBtn);
@@ -49,6 +54,9 @@ namespace card {
 		infoBtn->setBackground(std::make_shared<egui::TexturedBackground>(
 			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\information.png")
 		));
+		infoBtn->setHoveredBackground(std::make_shared<egui::TexturedBackground>(
+			egui::Image::loadTexture(card::getApplicationFolder() + "\\resources\\menu\\informationHover.png")
+		), egui::RenderMode::RENDER_EXCLUSIVELY);
 	}
 	void MainMenuElement::addOnSinglePlayerBtnHandler(egui::FunctionWrapper<egui::ActionEvent> callback) {
 		auto convertedCallback = convertEventHandler(callback);
