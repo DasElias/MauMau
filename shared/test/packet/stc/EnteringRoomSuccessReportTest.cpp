cbdbc3dd37084b54313de11926dc486894319061
Elias Horner <elias.horner@student.htldornbirn.at>
2020-02-26 19:21:52 +0100

    fix not compiling unit test

--- shared/test/packet/stc/EnteringRoomSuccessReportTest.cpp
+++ shared/test/packet/stc/EnteringRoomSuccessReportTest.cpp
@@ -7,11 +7,12 @@ TEST_CASE("EnteringRoomSuccessReport_STCAnswerPacket", "[EnteringRoomSuccessRepo
 	RoomCode const roomCode = 10;
 	std::vector<std::string> const usernamesOfOtherParticipants = {{"Hey", "Test", "Hello World"}};
 	std::vector<Avatar> const avatarsOfOtherParticipants = {{4, 6, 8}};
+	std::vector<bool> const areOtherParticipantsAiPlayers = {{false, false}};
 	std::string const roomLeader = "roomLeader"; 
 	std::map<std::string, int> options;
 	options["Test"] = 1;
 
-	EnteringRoomSuccessReport_STCAnswerPacket packet(statusCode, usernamesOfOtherParticipants, avatarsOfOtherParticipants, roomLeader, roomCode, options);
+	EnteringRoomSuccessReport_STCAnswerPacket packet(statusCode, usernamesOfOtherParticipants, avatarsOfOtherParticipants, areOtherParticipantsAiPlayers, roomLeader, roomCode, options);
 	nlohmann::json json = nlohmann::json::parse(packet.getJson());
 	EnteringRoomSuccessReport_STCAnswerPacket jsonPacket(json);
 
@@ -19,6 +20,7 @@ TEST_CASE("EnteringRoomSuccessReport_STCAnswerPacket", "[EnteringRoomSuccessRepo
 	REQUIRE(roomCode == jsonPacket.getRoomCode());
 	REQUIRE(usernamesOfOtherParticipants == jsonPacket.getUsernamesOfOtherParticipants());
 	REQUIRE(avatarsOfOtherParticipants == jsonPacket.getAvatarsOfOtherParticipants());
+	REQUIRE(areOtherParticipantsAiPlayers == jsonPacket.areOtherParticipantsAiPlayers());
 	REQUIRE(roomLeader == jsonPacket.getRoomLeader());
 	REQUIRE(options == jsonPacket.getOptions());
 } 
