a7396f78c6fd9226fc1139062c074e0138e8a1bd
Elias Horner <elias.horner@student.htldornbirn.at>
2020-03-27 16:03:35 +0100

    wrap json parsing in try-catch-block on server side

--- shared/shared/packet/CTSPacketConstructorFromJson.cpp
+++ shared/shared/packet/CTSPacketConstructorFromJson.cpp
@@ -24,23 +24,28 @@
 
 namespace card {
 	void constructAndProcessCTSPacketFromJson(std::string& json, std::function<void(ClientToServerPacket&)> callback) {
-		nlohmann::json jsonHandle = nlohmann::json::parse(json);
-		int packetType = Packet::getPacketType(jsonHandle);
+		try {
+			nlohmann::json jsonHandle = nlohmann::json::parse(json);
 
-		switch(packetType) {
-			PACKET_TYPE_CASE(ChangeOptionsRequest_CTSPacket)
-			PACKET_TYPE_CASE(ChangeRoomLeaderRequest_CTSPacket)
-			PACKET_TYPE_CASE(DrawCardRequest_CTSPacket)
-			PACKET_TYPE_CASE(GameStartRequest_CTSPacket)
-			PACKET_TYPE_CASE(MauRequest_CTSPacket)
-			PACKET_TYPE_CASE(PlayCardRequest_CTSPacket)
-			PACKET_TYPE_CASE(RoomCreationRequest_CTSPacket)
-			PACKET_TYPE_CASE(RoomJoinRequest_CTSPacket)
-			PACKET_TYPE_CASE(KickPlayerRequest_CTSPacket)
-			PACKET_TYPE_CASE(JoinAiPlayerRequest_CTSPacket)
-			PACKET_TYPE_CASE(PassRequest_CTSPacket)
-			default:
-				log(LogSeverity::ERR, "Packet with unknown packet id (" + std::to_string(packetType) + ") received!");
+			int packetType = Packet::getPacketType(jsonHandle);
+			switch(packetType) {
+				PACKET_TYPE_CASE(ChangeOptionsRequest_CTSPacket)
+					PACKET_TYPE_CASE(ChangeRoomLeaderRequest_CTSPacket)
+					PACKET_TYPE_CASE(DrawCardRequest_CTSPacket)
+					PACKET_TYPE_CASE(GameStartRequest_CTSPacket)
+					PACKET_TYPE_CASE(MauRequest_CTSPacket)
+					PACKET_TYPE_CASE(PlayCardRequest_CTSPacket)
+					PACKET_TYPE_CASE(RoomCreationRequest_CTSPacket)
+					PACKET_TYPE_CASE(RoomJoinRequest_CTSPacket)
+					PACKET_TYPE_CASE(KickPlayerRequest_CTSPacket)
+					PACKET_TYPE_CASE(JoinAiPlayerRequest_CTSPacket)
+					PACKET_TYPE_CASE(PassRequest_CTSPacket)
+				default:
+					log(LogSeverity::ERR, "Packet with unknown packet id (" + std::to_string(packetType) + ") received!");
+			}
+		} catch(nlohmann::json::parse_error& pe) {
+			log(LogSeverity::ERR, "Parsing error! \"" + std::string(pe.what()) + "\"");
 		}
+
 	}
 }
