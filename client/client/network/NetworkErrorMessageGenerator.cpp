48a8c342616bd88ab5dc0903e1cc9d859af713b8
Elias Horner <elias.horner@student.htldornbirn.at>
2020-04-26 14:08:34 +0200

    add case in NetworkErrorMessageGenerator

--- client/client/network/NetworkErrorMessageGenerator.cpp
+++ client/client/network/NetworkErrorMessageGenerator.cpp
@@ -8,6 +8,7 @@ namespace card {
 			case boost::asio::error::timed_out:
 				return "Verbindung zum Server konnte nicht hergestellt werden. Bitte versuche es später erneut.";
 			case boost::system::errc::no_link:
+			case boost::asio::error::connection_aborted:
 				return "Deine Internetverbindung ging verloren. Bitte versuche es später erneut.";
 			default:
 				return "Keine Internetverbindung. Bitte versuche es später erneut.";
